9371f3ed0d1c006d1ff89da2797c6e5837c970fa
Prashik Kamble <kambleprashik7349@gmail.com>
2022-11-08 15:38:46 +0530

    ADDED : code for display default values

--- JavaFundamentals/src/main/java/org/example/Main.java
+++ JavaFundamentals/src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
+    String stringValue;
+    int intValue;
+    float floatValue;
+    boolean booleanValue;
+    double doubleValue;
+    long longValue;
+
+    public void defaultValues() {
+        System.out.println("All default values");
+        System.out.println("String : " + stringValue);
+        System.out.println("Integer : " + intValue);
+        System.out.println("Float : " + floatValue);
+        System.out.println("Boolean : " + booleanValue);
+        System.out.println("Double : " + doubleValue);
+        System.out.println("Long : " + longValue);
+    }
+
+    public void displayMessage() {
         System.out.println("Welcome!");
+    }
+
+    public static void main(String[] args) {
+        Main mainObj = new Main();
+        mainObj.displayMessage();
+        mainObj.defaultValues();
     }
 }
